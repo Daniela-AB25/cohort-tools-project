9c6f5804593e5bf54b724795efa89ba6bdb366a1
Daniela Astorga Bilbao <25daniela99ab@gmail.com>
2024-05-20 16:19:58 +0200

    Routes created

--- server/app.js
+++ server/app.js
@@ -6,6 +6,8 @@ const PORT = 5005;
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
+const cohortsData = require('./cohorts.json')
+const studentsData = require('./students.json')
 
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
@@ -28,6 +30,14 @@ app.use(cookieParser());
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
+
+app.get('/api/cohorts', (req, res) => {
+  res.json(cohortsData)
+});
+
+app.get('/api/students', (req, res) => {
+  res.json(studentsData)
+})
 
 
 // START SERVER
